9194fef2ebe049b4d5201207fafb9cac30d64674
vohraaYatinn <vohrayatin9@gmail.com>
2024-04-24 00:41:34 +0530

    ss

--- django-app/backendAuto/settings.py
+++ django-app/backendAuto/settings.py
@@ -78,6 +78,8 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:3000",
+        "http://www.optiprimefulfillment.com",
+
     "https://www.optiprimefulfillment.com",
     "https://optiprimefulfillment.com",
     "https://optiprimefulfillment.com:3000",
@@ -178,8 +180,8 @@ AUTHENTICATION_BACKENDS = [
 
 # CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:3030']
 # CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3030']
-CSRF_TRUSTED_ORIGINS = ["https://www.optiprimefulfillment.com",  "http://127.0.0.1:3000", "http://145.14.158.251:3000"]
-CORS_ORIGIN_WHITELIST =  ["https://www.optiprimefulfillment.com",  "http://127.0.0.1:3000", "http://145.14.158.251:3000"]
+CSRF_TRUSTED_ORIGINS = ["http://www.optiprimefulfillment.com",  "http://127.0.0.1:3000", "http://145.14.158.251:3000"]
+CORS_ORIGIN_WHITELIST =  ["http://www.optiprimefulfillment.com",  "http://127.0.0.1:3000", "http://145.14.158.251:3000"]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATIC_URL = '/static/'
